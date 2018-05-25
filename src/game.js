4922944d8aab3ae5053ccc6fbf3c94f455e8ecb1
vanillafrosty <dwu.inq@gmail.com>
2018-05-25 18:52:40 -0400

    refactor audio toggle to call the main music toggle

--- src/game.js
+++ src/game.js
@@ -8,19 +8,46 @@ function pauseAudio() {
 
 const toggleButton = document.getElementById("toggleAudio");
 
+// const toggleAudio = () => {
+//   let megamanPlaying = false;
+//   return (e) => {
+//     e.preventDefault();
+//     const megaman = document.getElementById("megaman");
+//     if (!megamanPlaying) {
+//       megaman.play();
+//       megamanPlaying = true;
+//     } else {
+//       megaman.pause();
+//       megamanPlaying = false;
+//     }
+//   }
+// };
+
 const toggleAudio = () => {
-  let audioPlaying = false;
+  let megamanPlaying = false;
   return (e) => {
     e.preventDefault();
-    const megaman = document.getElementById("megaman");
-    if (!audioPlaying) {
-      megaman.play();
-      audioPlaying = true;
-    } else {
-      megaman.pause();
-      audioPlaying = false;
-    }
+    megamanPlaying = toggleMegaman(megamanPlaying);
   }
 };
 
+
 toggleButton.addEventListener("click", toggleAudio());
+
+const toggleMegaman = (playing) => {
+  const megaman = document.getElementById("megaman");
+  if (!playing) {
+    megaman.play();
+    return true;
+  } else {
+    megaman.pause();
+    return false;
+  }
+};
+
+
+// const titlePlaying = true;
+//
+// const toggleTitle = () => {
+//
+// }
