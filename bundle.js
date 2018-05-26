ebe9bfc1a4458eee1ff391bd9510c15ff80d4a23
vanillafrosty <dwu.inq@gmail.com>
2018-05-26 15:39:51 -0400

    add canvas, update styling

--- bundle.js
+++ bundle.js
@@ -102,6 +102,20 @@ document.addEventListener('DOMContentLoaded', function () {
   titleAudio.addEventListener("ended", function () {
     game.titleEnded = true;
   });
+  var canvas = document.getElementById("canvas");
+  canvas.width = 300;
+  canvas.height = 720;
+  var ctx = canvas.getContext('2d');
+  // ctx.fillStyle = 'rgb(200,0,0)';
+  // ctx.fillRect(0, 0, 700, 700);
+
+  ctx.beginPath();
+  ctx.strokeStyle = 'blue';
+  ctx.arc(100, 100, 50, 0, 360);
+  ctx.lineWidth = 2;
+  ctx.fillStyle = 'rgb(200,0,0)';
+  ctx.fill();
+  ctx.stroke();
 });
 
 /***/ }),
--- src/app.js
+++ src/app.js
@@ -16,5 +16,19 @@ document.addEventListener('DOMContentLoaded', () => {
   titleAudio.addEventListener("ended", () => {
     game.titleEnded = true;
   });
+  const canvas = document.getElementById("canvas");
+  canvas.width = 300;
+  canvas.height = 720;
+  var ctx = canvas.getContext('2d');
+  // ctx.fillStyle = 'rgb(200,0,0)';
+  // ctx.fillRect(0, 0, 700, 700);
+
+  ctx.beginPath();
+  ctx.strokeStyle = 'blue';
+  ctx.arc(100, 100, 50, 0, 360);
+  ctx.lineWidth = 2;
+  ctx.fillStyle = 'rgb(200,0,0)';
+  ctx.fill();
+  ctx.stroke();
 
 });
